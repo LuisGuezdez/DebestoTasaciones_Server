9a9928b3abd51d91c4a22c76f21f5e59f6cf89de
Luis Rodríguez <isguezdez@gmail.com>
2022-12-15 21:27:25 +0100

    hechas las entidades de tasacion, usuario y tipo-usuario

--- src/main/java/net/ausiasmarch/debesto/entity/TasacionEntity.java
+++ src/main/java/net/ausiasmarch/debesto/entity/TasacionEntity.java
@@ -1,18 +1,24 @@
 package net.ausiasmarch.debesto.entity;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
-@Table(name = "developer")
+@Table(name = "tasacion")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class TasacionEntity {
     
@@ -26,6 +32,79 @@ public class TasacionEntity {
     private Boolean asignado;
     private Double precio;
 
+    @OneToMany(mappedBy = "tasacion", fetch = FetchType.LAZY)
+    private final List<CocheEntity> coches;
     
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_sucursal")
+    private SucursalEntity sucursal;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_usuario")
+    private SucursalEntity usuario;
+
+    public TasacionEntity() {
+
+        this.coches = new ArrayList<>();
+    }
+
+    public TasacionEntity(Long id) {
+
+        this.coches = new ArrayList<>();
+        this.id = id; //Ojo, no se si se deberia dejar aqui este this.id
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getReserva() {
+        return reserva;
+    }
+
+    public void setReserva(LocalDateTime reserva) {
+        this.reserva = reserva;
+    }
+
+    public Boolean getAsignado() {
+        return asignado;
+    }
+
+    public void setAsignado(Boolean asignado) {
+        this.asignado = asignado;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public List<CocheEntity> getCoches() {
+        return coches;
+    }
+
+    public SucursalEntity getSucursal() {
+        return sucursal;
+    }
+
+    public void setSucursal(SucursalEntity sucursal) {
+        this.sucursal = sucursal;
+    }
+
+    public SucursalEntity getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(SucursalEntity usuario) {
+        this.usuario = usuario;
+    }
+
     
 }
