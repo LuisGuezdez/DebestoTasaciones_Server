c790a47120542eea1c2b4b751bec6212f918f064
Luis Rodríguez <isguezdez@gmail.com>
2023-01-25 20:08:48 +0100

    empezado la tabla de sucursal

--- src/main/java/net/ausiasmarch/debesto/api/SucursalController.java
+++ src/main/java/net/ausiasmarch/debesto/api/SucursalController.java
@@ -10,6 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,11 +32,25 @@ public class SucursalController {
         return new ResponseEntity<>(oSucursalService.get(id), HttpStatus.OK);
     }
 
+    @GetMapping("/count")
+    public ResponseEntity<Long> count() {
+        return new ResponseEntity<Long>(oSucursalService.count(), HttpStatus.OK);
+    }
+
     @GetMapping
     public ResponseEntity<Page<SucursalEntity>> getPage(
             @ParameterObject @PageableDefault(page = 0, size = 10, direction = Sort.Direction.DESC) Pageable oPageable,
             @RequestParam(name = "filter", required = false) String strFilter) {
         return new ResponseEntity<>(oSucursalService.getPage(oPageable, strFilter), HttpStatus.OK);
     }
+
+    @PutMapping
+    public ResponseEntity<Long> update(@RequestBody SucursalEntity oUsuarioEntity) {
+        return new ResponseEntity<Long>(oSucursalService.update(oUsuarioEntity), HttpStatus.OK);
+    }
     
+    @PostMapping
+    public ResponseEntity<Long> create(@RequestBody SucursalEntity oNewUsuarioEntity) {
+        return new ResponseEntity<Long>(oSucursalService.create(oNewUsuarioEntity), HttpStatus.OK);
+    }
 }
--- src/main/java/net/ausiasmarch/debesto/service/SucursalService.java
+++ src/main/java/net/ausiasmarch/debesto/service/SucursalService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import net.ausiasmarch.debesto.entity.SucursalEntity;
 import net.ausiasmarch.debesto.exception.ResourceNotFoundException;
+import net.ausiasmarch.debesto.exception.ValidationException;
 import net.ausiasmarch.debesto.helper.ValidationHelper;
 import net.ausiasmarch.debesto.repository.SucursalRepository;
 
@@ -16,9 +17,22 @@ public class SucursalService {
     @Autowired
     SucursalRepository oSucursalRepository;
 
+    @Autowired
+    AuthService oAuthService;
+
     public void validate(Long id) {
         if (!oSucursalRepository.existsById(id)) {
             throw new ResourceNotFoundException("id " + id + " not exist");
+        }
+    }
+
+    public void validate(SucursalEntity oSucursalEntity) {
+        ValidationHelper.validateStringLength(oSucursalEntity.getNombre(), 2, 70, "campo nombre de Usuario(el campo debe tener longitud de 2 a 70 caracteres)");
+        ValidationHelper.validateStringLength(oSucursalEntity.getLocalidad(), 2, 70, "campo apellidos de Usuario(el campo debe tener longitud de 2 a 70 caracteres)");
+        ValidationHelper.validateStringLength(oSucursalEntity.getCalle(), 2, 70, "campo apellidos de Usuario(el campo debe tener longitud de 2 a 70 caracteres)");
+        ValidationHelper.validateRange(oSucursalEntity.getPostal(), 100000, 99999, "campo postal de Sucursal(el campo debe tener 5 dígitos");
+        if (oSucursalRepository.existsByNombre(oSucursalEntity.getNombre())) {
+            throw new ValidationException("el campo nombre está repetido");
         }
     }
 
@@ -26,6 +40,11 @@ public class SucursalService {
         //oAuthService.OnlyAdminsOrOwnUsersData(id);
         return oSucursalRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Sucursal with id: " + id + " not found"));
+    }
+
+    public Long count() {
+        //oAuthService.OnlyAdmins();
+        return oSucursalRepository.count();
     }
 
     public Page<SucursalEntity> getPage(Pageable oPageable, String strFilter) {
@@ -36,6 +55,20 @@ public class SucursalService {
         }else{
             return oSucursalRepository.findByNombreIgnoreCaseContainingOrLocalidadIgnoreCaseContaining(strFilter, strFilter, oPageable);
         }
+    }
+
+    public Long update(SucursalEntity oSucursalEntity) {
+        // oAuthService.OnlyAdminsOrOwnUsersData(oSucursalEntity.getId());
+         validate(oSucursalEntity.getId());
+         return oSucursalRepository.save(oSucursalEntity).getId();
+     }
+
+     public Long create(SucursalEntity oNewSucursalEntity) {
+        //oAuthService.OnlyAdmins();
+        validate(oNewSucursalEntity);
+        oNewSucursalEntity.setId(0L);
+        oNewSucursalEntity.setContraseña(DEBESTO_DEFAULT_PASSWORD);
+        return oSucursalRepository.save(oNewSucursalEntity).getId();
     }
     
 }
